c6bb8c222ad8c03685e2de1df069dc2fd7cacf9b
David Jackson <jack8679@kettering.edu>
2018-02-21 16:43:57 -0500

    Added coordinated Bottom Navigation and ViewPager
    ViewPager and Bottom Nav have 5 screens for each of the 5 GTD steps (capture, process, organize, review, engage).

--- GTDApp/app/src/main/java/coders/mich/gtdapp/TestMainActivity.java
+++ GTDApp/app/src/main/java/coders/mich/gtdapp/TestMainActivity.java
@@ -13,10 +13,16 @@ public class TestMainActivity extends AppCompatActivity {
 
     private static final String TAG = "TestMainActivity";
 
+    private MainNavManager navManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_main);
+
+        navManager = new MainNavManager(this,
+                findViewById(R.id.navigation), findViewById(R.id.view_pager_content));
+
     }
 
 }
