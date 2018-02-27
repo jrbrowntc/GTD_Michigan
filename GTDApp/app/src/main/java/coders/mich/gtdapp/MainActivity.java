b165ff66822ce347cb7b05311be5fdabe04df4f0
David Jackson <jack8679@kettering.edu>
2018-02-27 08:43:05 -0500

    Simplified method for opening activities from MainActivity
    Now opens the activity based on the view's tag

--- GTDApp/app/src/main/java/coders/mich/gtdapp/MainActivity.java
+++ GTDApp/app/src/main/java/coders/mich/gtdapp/MainActivity.java
@@ -4,35 +4,28 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button inboxButton;
-    private final static String inbox = "inbox";
+
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        inboxButton = findViewById(R.id.btn_inbox);
-        inboxButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Context context = view.getContext();
-                Class destinationClass = InboxActivity.class;
-                Intent intentToStartDetailActivity = new Intent(context, destinationClass);
-                intentToStartDetailActivity.putExtra(Intent.EXTRA_TEXT, inbox);
-                startActivity(intentToStartDetailActivity);
-            }
-        });
+    }
 
-        findViewById(R.id.btn_test_main_activity).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent testMainActivityIntent = new Intent(getApplicationContext(),
-                        TestMainActivity.class);
-                startActivity(testMainActivityIntent);
-            }
-        });
+    public void openActivityBasedOnTag(View view) {
+        String className = getPackageName() + "." + view.getTag();
+        try {
+            Intent activityIntent = new Intent(this,
+                        Class.forName(className));
+            startActivity(activityIntent);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
