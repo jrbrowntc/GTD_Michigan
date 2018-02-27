1f5a4334c0a193dbde534633dec4412c95dfedab
David Jackson <jack8679@kettering.edu>
2018-02-27 16:20:52 -0500

    Added ability to populate side drawer sub menu dynamically
    This way we can fill it with data from our database

--- GTDApp/app/src/main/java/coders/mich/gtdapp/TestMain2Activity.java
+++ GTDApp/app/src/main/java/coders/mich/gtdapp/TestMain2Activity.java
@@ -16,8 +16,14 @@ import android.transition.AutoTransition;
 import android.transition.TransitionManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SubMenu;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.List;
+
+import coders.mich.gtdapp.data.DummyData;
+import coders.mich.gtdapp.data.dao.Bucket;
 
 public class TestMain2Activity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -27,6 +33,8 @@ public class TestMain2Activity extends AppCompatActivity
 
     private FloatingActionButton fab;
     private AnimatedVectorDrawable avdAddToDone, avdDoneToAdd;
+
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,8 +64,10 @@ public class TestMain2Activity extends AppCompatActivity
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        fillMenu();
     }
 
     @Override
@@ -151,5 +161,25 @@ public class TestMain2Activity extends AppCompatActivity
         }
 
         modalVisible = !modalVisible;
+    }
+
+    public void fillMenu() {
+        Menu menu = navigationView.getMenu();
+        SubMenu subMenu = menu.addSubMenu("Buckets");
+
+        // TODO: 2/27/2018 Replace with Room DAO Method to get all buckets
+        List<Bucket> buckets = DummyData.getBuckets();
+
+        for (int i = 0; i < buckets.size(); i++) {
+            Bucket bucket = buckets.get(i);
+            String bucketName = bucket.getName();
+            Integer iconId = bucket.getIconId();
+
+            if (iconId == null) iconId = R.drawable.ic_folder_black_24dp;
+
+            subMenu.add(0, Menu.FIRST + i, Menu.FIRST, bucketName)
+                    .setCheckable(true)
+                    .setIcon(iconId);
+        }
     }
 }
